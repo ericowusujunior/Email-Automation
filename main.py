6dd560d7fe550e92ac586b7a6947f03c61a9804d
ericowusujunior <e.owusu89@gmail.com>
2022-02-16 00:53:11 +0000

    Chnaged the receivers email address to a temporary email

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import yagmail
 import os
+import time
+
 "This was to send a single email"
 
 sender = "e.owusu89@gmail.com"
-receiver = "e.owusu@dal.ca"
+receiver = "1yr89yqf@spymail.one"
 
 subject = "Automated Trial Email"
 
-content = """" I am writing this email to see how it goes"""
+content = """"Sending email using python"""
 
-yag = yagmail.SMTP(user = sender,password = os.getenv('PASSWORD'))
+while True:
 
-yag.send(to=receiver, subject = subject, contents = content)
+  yag = yagmail.SMTP(user = sender,password = os.getenv('PASSWORD'))
 
-print("Email Sent!")
-my_secret = os.environ['PASSWORD']
+  yag.send(to=receiver, subject = subject, contents = content)
+  my_secret = os.environ['PASSWORD']
+  print("Email Sent!")
+  time.sleep(60)
+
+
 
 
