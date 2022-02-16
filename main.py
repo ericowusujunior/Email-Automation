e883d1e03e4397e11b73fbc8ac02f4a508b887fa
ericowusujunior <e.owusu89@gmail.com>
2022-02-16 01:58:38 +0000

    Modified the email to be sent everyday at a specific time

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import yagmail
 import os
 import time
+from datetime import datetime 
 
-"This was to send a single email"
+"This sends an email to the receiver everyday at 1:50"
 
 sender = "e.owusu89@gmail.com"
 receiver = "1yr89yqf@spymail.one"
@@ -12,13 +13,14 @@ subject = "Automated Trial Email"
 content = """Sending email using python"""
 
 while True:
+  now = datetime.now()
+  if now.hour == 1 and now.minute == 55:
+    yag = yagmail.SMTP(user = sender,password = os.getenv('PASSWORD'))
 
-  yag = yagmail.SMTP(user = sender,password = os.getenv('PASSWORD'))
-
-  yag.send(to=receiver, subject = subject, contents = content)
-  my_secret = os.environ['PASSWORD']
-  print("Email Sent!")
-  time.sleep(60)
+    yag.send(to=receiver, subject = subject, contents = content)
+    my_secret = os.environ['PASSWORD']
+    print("Email Sent!")
+    time.sleep(60)
 
 
 
